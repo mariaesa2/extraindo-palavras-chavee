0d8c6303b4dd9fe1c26ad21664b0e5576e785b3e
mariaesa2 <maria.santos.alves07@escola.pr.gov.br>
2025-07-03 19:11:00 +0000

    finalização- aula 5

---
+++ script.js
@@ -0,0 +1,31 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavra.addEventListener('click',mostraPalavrasChave);
+
+function mostraPalavrasChave (){
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
+
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+
+function processaTexto(texto){
+    let palavras = texto.split("/\P{L}+/u");
+
+    let frequencias = {};
+    for(let i of palavras){
+        frequencias[i] = 0;
+        for (let j of palavras){
+            if (i == j){
+                frequencias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+
+    return palavras;
+}
+
